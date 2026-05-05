9766d36772f0eafbb6ab089b40f3df4d381b77b9
germxz <gerardozuniga133@gmail.com>
2026-05-05 18:39:53 -0500

    resubmission updated

--- assignment3/list-comprehensions.py
+++ assignment3/list-comprehensions.py
@@ -7,9 +7,10 @@ with open("../csv/employees.csv", "r") as file:
     for row in reader:
         employees.append(row)
 
-names = [row['first_name'] + " " + row['last_name'] for row in employees]
+# Skip header row and use indices for first and last name
+names = [row[1] + " " + row[2] for row in employees[1:]]
 print(names)
 
+# Filter names that contain 'e'
 names_with_e = [name for name in names if "e" in name]
-
 print(names_with_e)
